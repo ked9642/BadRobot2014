8b40625a44d355b0f9fdff10ace3f7e26df73ea8
Kevin Edwards <ked9642@gmail.com>
2014-01-20 18:52:43 -0500

    added base of shooter position code

--- src/com/badrobot/subsystems/Shooter.java
+++ src/com/badrobot/subsystems/Shooter.java
@@ -8,6 +8,7 @@ import com.badrobot.OI;
 import com.badrobot.RobotMap;
 import com.badrobot.commands.Shoot;
 import com.badrobot.subsystems.interfaces.IShooter;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.SpeedController;
@@ -23,7 +24,12 @@ public class Shooter extends BadSubsystem implements IShooter
     
     Solenoid engageSolenoid, disengageSolenoid;
     SpeedController winch;
+    Encoder encoder;
+    DigitalInput opticalSensorTop, opticalSensorMiddle, opticalSensorBottom;
     
+    public int[] range = new int[4]; 
+    public int aboveTop = 0, topToMid = 1, midToBottom = 2;
+    public String area;
     public static Shooter getInstance()
     {
         if (instance == null)
@@ -57,11 +63,50 @@ public class Shooter extends BadSubsystem implements IShooter
 
     public void cockBack(double speed) 
     {
+        
+        if(opticalSensorBottom.get())
+        {
+            area = "belowBottom";
+            range[midToBottom] =  encoder.get();
+            encoder.reset();
+            encoder.start();
+        }
+        else if(opticalSensorMiddle.get())
+        {
+            area = "midToBottom";
+            range[topToMid] = encoder.get();
+            encoder.reset();
+            encoder.start();
+        }
+        else if(opticalSensorTop.get())
+        {
+            area = "topToMid";
+            range[aboveTop] = encoder.get();
+            encoder.start();    
+        }
+        
         winch.set(-speed);   
+    }
+    
+    /**
+     * 
+     * @param area
+     * @return 
+     */
+    public int getRangeLength(int area)
+    {
+        return range[area];
+    }
+    
+    public String getCurrentArea()
+    {
+        return area;
     }
 
     public void disengageWinch() 
     {
+        encoder.reset();
+        area = "aboveTop";
         engageSolenoid.set(false);
         disengageSolenoid.set(true);
     }
